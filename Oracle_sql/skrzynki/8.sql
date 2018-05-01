9754785bc6b5f6fc980cc6f155775542851392ff
iKlaKier <38748061+iKlaKier@users.noreply.github.com>
2018-05-01 15:22:04 +0200

    asd

--- Oracle_sql/skrzynki/8.sql
+++ Oracle_sql/skrzynki/8.sql
@@ -35,3 +35,56 @@ CREATE TABLE produkt(
 --
 alter table klient
     modify(customerID constraint re_klient2zamowienia references zamowienia(IDcustomer) UNIQUE);
+
+alter table pozycja
+    add constraint rel_produkt2pozycja
+    FOREIGN KEY (IDproduct) REFERENCES produkt(productID);
+	
+alter table pozycja		-- relecja pozycji do zmówień
+    add constraint rel_zamowienia2pozycja
+    FOREIGN KEY (IDorder) REFERENCES zamowienia(orderID);
+	
+alter table zamowienia
+    add constraint rel_klient2zamowienia
+    FOREIGN KEY (IDcustomer) REFERENCES klient(customerID);
+	
+alter table pozycja
+    modify(IDorder unique);
+	
+	
+-- 2
+alter table klient
+    add(mail varchar2(45) null);
+-- 2.2
+alter table klient
+    rename column addr_postacode to addr_postalcode;
+
+alter table klient
+    modify addr_postalcode varchar2(7);
+--
+-- 2.3
+alter table zamowienia
+    add(Filled varchar2(3) check(Filled in ('Yes', 'No')));
+-- 2.4
+alter table zamowienia
+    add(FillDate DATE null);
+-- 2.5
+alter table zamowienia
+    rename column Filled to Status;
+alter table zamowienia
+    modify(Status varchar2(21) check(Status in ('Nowe zamówienie', 'Realizowane', 'Przesyłka wysłana', 'Realizacja
+zakończona')));
+
+-- 2.6
+ALTER TABLE produkt
+modify(price_gross as default(price_net*1.23) VIRTUAL);
+
+-- 2.7
+create index client_fast
+    ON klient(surname, login, mail);
+	
+-- 2.8
+create unique index client_fast
+    ON klient(surname, login, mail);
+
+
